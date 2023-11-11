d3f43d60d148115fa60ac3b22c9f289404657fb9
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-11-11 10:05:37 +0100

    #24: Activate generating documentation

--- RefleCS/RefleCS/ICsFileHandler.cs
+++ RefleCS/RefleCS/ICsFileHandler.cs
@@ -4,8 +4,18 @@ namespace RefleCS;
 
 public interface ICsFileHandler
 {
+    /// <summary>
+    /// Returns null if the file's content is not valid C# code or C# code that cannot be converted to a CsFile.
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
     CsFile? FromFile(string filePath);
 
+    /// <summary>
+    /// Returns null if the content is not valid C# code or C# code that cannot be converted to a CsFile.
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns></returns>
     CsFile? FromCode(string content);
 
     void SaveOrReplace(CsFile csFile, string filePath);
