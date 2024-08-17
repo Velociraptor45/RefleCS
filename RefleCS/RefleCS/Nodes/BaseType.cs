d82d72cc61c2addc075347ba39384d72c116bbff
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-08-17 09:13:59 +0200

    #22: Add summaries to classes

--- RefleCS/RefleCS/Nodes/BaseType.cs
+++ RefleCS/RefleCS/Nodes/BaseType.cs
@@ -1,3 +1,7 @@
 ﻿namespace RefleCS.Nodes;
 
-public record BaseType(string Value);
+/// <summary>
+/// Represents a base type of a class. Can be a class or an interface.
+/// </summary>
+/// <param name="Name"></param>
+public record BaseType(string Name);
