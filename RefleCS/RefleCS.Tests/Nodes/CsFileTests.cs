cedb4917e0f8ed5b260fa496ecb61a7096d5ce30
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-08-17 09:02:36 +0200

    #22: Make CsFile methods chainable

--- RefleCS/RefleCS.Tests/Nodes/CsFileTests.cs
+++ RefleCS/RefleCS.Tests/Nodes/CsFileTests.cs
@@ -6,14 +6,183 @@ namespace RefleCS.Tests.Nodes;
 
 public class CsFileTests
 {
+    public class AddUsing
+    {
+        private readonly AddUsingFixture _fixture = new();
+
+        [Fact]
+        public void AddUsing_WithExistingUsing_ShouldNotAddUsing()
+        {
+            // Arrange
+            _fixture.SetupNamespaceEmpty();
+            _fixture.SetupExistingUsing();
+            var sut = _fixture.CreateSut();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExistingUsing);
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
+
+            // Act
+            var result = sut.AddUsing(_fixture.ExistingUsing);
+
+            // Assert
+            result.Should().Be(sut);
+            sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult);
+        }
+
+        [Fact]
+        public void AddUsing_WithNonExistingUsing_ShouldAddUsing()
+        {
+            // Arrange
+            _fixture.SetupNamespaceEmpty();
+            _fixture.SetupNonExistingUsing();
+            var sut = _fixture.CreateSut();
+
+            var list = sut.Usings;
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.NonExistingUsing);
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
+
+            // Act
+            var result = sut.AddUsing(_fixture.NonExistingUsing);
+
+            // Assert
+            result.Should().Be(sut);
+            sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult);
+            ReferenceEquals(sut.Usings, list).Should().BeTrue();
+        }
+
+        private sealed class AddUsingFixture : CsFileFixture
+        {
+            public Using? ExistingUsing { get; private set; }
+            public Using? NonExistingUsing { get; private set; }
+            public IReadOnlyCollection<Using>? ExpectedResult { get; private set; }
+
+            public void SetupExistingUsing()
+            {
+                Usings = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+
+                ExistingUsing = new("a");
+
+                ExpectedResult = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+            }
+
+            public void SetupNonExistingUsing()
+            {
+                Usings = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+
+                NonExistingUsing = new("c");
+
+                ExpectedResult = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                    new("c"),
+                };
+            }
+        }
+    }
+
+    public class RemoveUsing
+    {
+        private readonly RemoveUsingFixture _fixture = new();
+
+        [Fact]
+        public void RemoveUsing_WithExistingUsing_ShouldRemoveUsing()
+        {
+            // Arrange
+            _fixture.SetupNamespaceEmpty();
+            _fixture.SetupExistingUsing();
+            var sut = _fixture.CreateSut();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExistingUsing);
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
+
+            // Act
+            var result = sut.RemoveUsing(_fixture.ExistingUsing);
+
+            // Assert
+            result.Should().Be(sut);
+            sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult);
+        }
+
+        [Fact]
+        public void RemoveUsing_WithNonExistingUsing_ShouldNotRemoveUsing()
+        {
+            // Arrange
+            _fixture.SetupNamespaceEmpty();
+            _fixture.SetupNonExistingUsing();
+            var sut = _fixture.CreateSut();
+
+            var list = sut.Usings;
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.NonExistingUsing);
+            TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
+
+            // Act
+            var result = sut.RemoveUsing(_fixture.NonExistingUsing);
+
+            // Assert
+            result.Should().Be(sut);
+            sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult);
+            ReferenceEquals(sut.Usings, list).Should().BeTrue();
+        }
+
+        private sealed class RemoveUsingFixture : CsFileFixture
+        {
+            public Using? ExistingUsing { get; private set; }
+            public Using? NonExistingUsing { get; private set; }
+            public IReadOnlyCollection<Using>? ExpectedResult { get; private set; }
+
+            public void SetupExistingUsing()
+            {
+                Usings = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+
+                ExistingUsing = new("a");
+
+                ExpectedResult = new List<Using>
+                {
+                    new("b"),
+                };
+            }
+
+            public void SetupNonExistingUsing()
+            {
+                Usings = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+
+                NonExistingUsing = new("c");
+
+                ExpectedResult = new List<Using>
+                {
+                    new("a"),
+                    new("b"),
+                };
+            }
+        }
+    }
+
     public class OrderUsingsAsc
     {
-        private readonly OrderUsingsAscFixture _fixture;
-
-        public OrderUsingsAsc()
-        {
-            _fixture = new OrderUsingsAscFixture();
-        }
+        private readonly OrderUsingsAscFixture _fixture = new();
 
         [Fact]
         public void OrderUsingsAsc_WithMultipleUsings_ShouldOrderUsings()
@@ -26,9 +195,10 @@ public class CsFileTests
             TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
 
             // Act
-            sut.OrderUsingsAsc();
+            var result = sut.OrderUsingsAsc();
 
             // Assert
+            result.Should().Be(sut);
             sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult, cfg => cfg.WithStrictOrdering());
         }
 
@@ -45,9 +215,10 @@ public class CsFileTests
             var list = sut.Usings;
 
             // Act
-            sut.OrderUsingsAsc();
+            var result = sut.OrderUsingsAsc();
 
             // Assert
+            result.Should().Be(sut);
             ReferenceEquals(sut.Usings, list).Should().BeTrue();
         }
 
@@ -78,12 +249,7 @@ public class CsFileTests
 
     public class OrderUsingsDesc
     {
-        private readonly OrderUsingsDescFixture _fixture;
-
-        public OrderUsingsDesc()
-        {
-            _fixture = new OrderUsingsDescFixture();
-        }
+        private readonly OrderUsingsDescFixture _fixture = new();
 
         [Fact]
         public void OrderUsingsDesc_WithMultipleUsings_ShouldOrderUsings()
@@ -96,9 +262,10 @@ public class CsFileTests
             TestPropertyNotSetException.ThrowIfNull(_fixture.ExpectedResult);
 
             // Act
-            sut.OrderUsingsDesc();
+            var result = sut.OrderUsingsDesc();
 
             // Assert
+            result.Should().Be(sut);
             sut.Usings.Should().BeEquivalentTo(_fixture.ExpectedResult, cfg => cfg.WithStrictOrdering());
         }
 
@@ -115,9 +282,10 @@ public class CsFileTests
             var list = sut.Usings;
 
             // Act
-            sut.OrderUsingsDesc();
+            var result = sut.OrderUsingsDesc();
 
             // Assert
+            result.Should().Be(sut);
             ReferenceEquals(sut.Usings, list).Should().BeTrue();
         }
 
@@ -148,7 +316,7 @@ public class CsFileTests
 
     private abstract class CsFileFixture
     {
-        public List<Using>? Usings;
+        protected List<Using>? Usings;
         private Namespace? _namespace;
 
         public CsFile CreateSut()
